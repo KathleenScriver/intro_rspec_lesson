3779f372868eb4d0465fd26dbdb1459a02123900
KathleenScriver <klescriver@gmail.com>
2019-08-21 10:59:37 -0600

    Fixes root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "/", to: "cats#index"
+  root to: "cats#index"
   resources :cats, only: [:index, :new, :create]
 end
