4b060ebf934c0fe3c81e62f03a587aedcd4a912f
KathleenScriver <klescriver@gmail.com>
2019-08-21 10:18:08 -0600

    Adds new cat test.

--- spec/features/cats_index_spec.rb
+++ spec/features/cats_index_spec.rb
@@ -12,4 +12,18 @@ describe 'Cats Index' do
     expect(page).to have_content(cat_2.name)
     expect(page).to have_content(cat_3.name)
   end
+
+  it 'makes a new cat' do
+    cat_1 = Cat.create(name: "Fluffy", breed: "Long-hair")
+    cat_2 = Cat.create(name: "Bob", breed: "Long-hair")
+
+    visit new_cat_path
+
+    fill_in :cat_name, with: "Meowface"
+    fill_in :cat_breed, with: "Siamese"
+    click_on "Create Cat"
+
+    expect(current_path).to eq(cats_path)
+    expect(page).to have_content("Meowface")
+  end
 end
